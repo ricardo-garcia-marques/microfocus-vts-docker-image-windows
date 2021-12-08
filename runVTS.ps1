f5c8bb10ede99e9fe0b7925ed0280ab562cac61e
Ricardo Garcia Marques <ricardogm@servicioexterno.inditex.com>
2021-12-08 10:46:25 +0100

    Add ciphers Enviroment variable

--- runVTS.ps1
+++ runVTS.ps1
@@ -11,6 +11,7 @@ $replace_use_ssl = '"useSSL": ' + $ENV:USE_SSL + ','
 $replace_default_language = '"defaultLanguage": "' + $ENV:DEFAULT_LANGUAGE + '",'
 $replace_tls_min_version = '"minVersion": "' + $ENV:TLS_MIN_VERSION + '",'
 $replace_tls_max_version = '"maxVersion": "' + $ENV:TLS_MAX_VERSION + '",'
+$replace_ciphers = '"ciphers": "' + $ENV:CIPHERS + '",'
 $replace_logger_level = '"level": "' + $ENV:LOGGER_LEVEL + '",'
 
 # Set VTS config
@@ -22,6 +23,7 @@ Replace('"autoCreateIndexedColumn": true,', $replace_auto_create_indexed_column)
 Replace('"defaultLanguage": "en",', $replace_default_language).
 Replace('"minVersion": "TLSv1.2",', $replace_tls_min_version).
 Replace('"maxVersion": "TLSv1.3",', $replace_tls_max_version).
+Replace('"ciphers": "ALL",', $replace_ciphers).
 Replace('"maxInstancesAllowed": 50,', $repalace_max_instances_allowed).
 Replace('"level": "error",', $replace_logger_level).
 Replace('"useSSL": false,', $replace_use_ssl ) |
